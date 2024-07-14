5b2a3e1a0ad80cf00317842077a1dc325406a732
Zane Bauman <zanebauman55@gmail.com>
2024-07-14 19:31:02 -0400

    PIO test

--- src/main.c
+++ src/main.c
@@ -1,25 +1,46 @@
-#include "hardware/gpio.h"
-#include "pico/binary_info.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-const uint LED_PIN = 2;
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
+// Our assembled program:
+#include "hello.pio.h"
 
 int main() {
-
-  bi_decl(bi_program_description("This is a test binary."));
-  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
-
   stdio_init_all();
 
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+  // Choose which PIO instance to use (there are two instances)
+  PIO pio = pio0;
 
-  while (1) {
-    gpio_put(LED_PIN, 0);
-    sleep_ms(250);
-    gpio_put(LED_PIN, 1);
-    puts("Hello World");
-    sleep_ms(5000);
+  // Our assembled program needs to be loaded into this PIO's instruction
+  // memory. This SDK function will find a location (offset) in the
+  // instruction memory where there is enough space for our program. We need
+  // to remember this location!
+  uint offset = pio_add_program(pio, &hello_program);
+
+  // Find a free state machine on our chosen PIO (erroring if there are
+  // none). Configure it to run our program, and start it, using the
+  // helper function we included in our .pio file.
+  uint sm1 = pio_claim_unused_sm(pio, true);
+  hello_program_init(pio, sm1, offset, 2);
+
+  // an another
+  uint sm2 = pio_claim_unused_sm(pio, true);
+  hello_program_init(pio, sm2, offset, 3);
+
+  // The state machine is now running. Any value we push to its TX FIFO will
+  // appear on the LED pin.
+  while (true) {
+    // Blink
+    pio_sm_put_blocking(pio, sm1, 1);
+    pio_sm_put_blocking(pio, sm2, 0);
+    sleep_ms(500);
+    // Blonk
+    pio_sm_put_blocking(pio, sm1, 0);
+    pio_sm_put_blocking(pio, sm2, 1);
+    sleep_ms(500);
   }
 }
