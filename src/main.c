7cdf89e91af5f59e9b92ff79e548884b8b23d8da
Zane Bauman <zanebauman55@gmail.com>
2024-07-14 19:38:56 -0400

    updates to PIO

--- src/main.c
+++ src/main.c
@@ -7,7 +7,22 @@
 #include "hardware/pio.h"
 #include "pico/stdlib.h"
 // Our assembled program:
-#include "hello.pio.h"
+#include "blink.pio.h"
+
+static inline void blink_pio_init(PIO pio, uint sm, uint offset, uint pin) {
+  pio_sm_config c = blink_program_get_default_config(offset);
+  // Map the state machine's OUT pin group to one pin, namely the `pin`
+  // parameter to this function.
+  sm_config_set_out_pins(&c, pin, 1);
+  // Set this pin's GPIO function (connect PIO to the pad)
+  pio_gpio_init(pio, pin);
+  // Set the pin direction to output at the PIO
+  pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
+  // Load our configuration, and jump to the start of the program
+  pio_sm_init(pio, sm, offset, &c);
+  // Set the state machine running
+  pio_sm_set_enabled(pio, sm, true);
+}
 
 int main() {
   stdio_init_all();
@@ -19,17 +34,17 @@ int main() {
   // memory. This SDK function will find a location (offset) in the
   // instruction memory where there is enough space for our program. We need
   // to remember this location!
-  uint offset = pio_add_program(pio, &hello_program);
+  uint offset = pio_add_program(pio, &blink_program);
 
   // Find a free state machine on our chosen PIO (erroring if there are
   // none). Configure it to run our program, and start it, using the
   // helper function we included in our .pio file.
   uint sm1 = pio_claim_unused_sm(pio, true);
-  hello_program_init(pio, sm1, offset, 2);
+  blink_pio_init(pio, sm1, offset, 2);
 
   // an another
   uint sm2 = pio_claim_unused_sm(pio, true);
-  hello_program_init(pio, sm2, offset, 3);
+  blink_pio_init(pio, sm2, offset, 3);
 
   // The state machine is now running. Any value we push to its TX FIFO will
   // appear on the LED pin.
